43f4d4f880e78ffa33d7a02cef73062f7bf696a4
alemlodyigor <szeregowygpim@gmail.com>
2023-11-24 15:24:51 +0100

    added login and ready state

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildMessageReactions,
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.GuildMembers,
+    Discord.GatewayIntentBits.GuildIntegrations,
+  ],
+});
+
+client.on("ready", () => {
+  console.log("Verification bot in online!");
+});
+
+client.login(process.env.TOKEN);
